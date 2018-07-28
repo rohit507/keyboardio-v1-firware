ca6c03d01696e032d2a0cd8ca77ae1bf55e2eb4e
Rohit Ramesh <rkr@berkeley.edu>
2018-07-28 13:19:55 -0700

    updated a few more keybinds

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -76,8 +76,7 @@
 // Support for turning active modifier keys white  
 #include "Kaleidoscope-LED-ActiveModColor.h"
 
-// A
-// #include "Kaleidoscope-LED-ActiveModColor.h"
+
 
 
 /** This 'enum' is a list of all the macros used by the Model 01's firmware
@@ -267,14 +266,14 @@ KEYMAPS(
    Key_Tab,  ___,              Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE,
    Key_Home, Key_mouseL,       Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,
    Key_End,  Key_PrintScreen,  Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  Key_mouseWarpSE,
-   Key_Tab, Key_Delete, ___, ___,
+   Key_Tab, Key_Spacebar, Key_Escape, ___,
    ___,
 
    Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,          Key_F10,          Key_F11,
    Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LeftCurlyBracket,     Key_RightCurlyBracket,    Key_LeftBracket, Key_RightBracket, Key_F12,
                                Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,              Key_RightArrow,  ___,              ___,
    Key_PcApplication,          Consumer_Mute,          Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,             Key_Backslash,    Key_Pipe,
-   ___, ___, Key_Enter, Key_Backspace,
+   ___, Key_Escape, Key_Enter, Key_Backspace,
    ___)
 	) // KEYMAPS(
 
@@ -422,12 +421,11 @@ KALEIDOSCOPE_INIT_PLUGINS(
   LEDControl,
 
   solidGreen,
-  // We start with the LED effect that turns off all the LEDs.
-  LEDOff,
+
 
   // The rainbow effect changes the color of all of the keyboard's keys at the same time
   // running through all the colors of the rainbow.
-  LEDRainbowEffect,
+  // LEDRainbowEffect,
 
   // The rainbow wave effect lights up your keyboard with all the colors of a rainbow
   // and slowly moves the rainbow across your keyboard
@@ -443,10 +441,13 @@ KALEIDOSCOPE_INIT_PLUGINS(
   solidViolet,
 
   // The breathe effect slowly pulses all of the LEDs on your keyboard
-  // LEDBreatheEffect,
+  // LEDBreatheEffect,asqwf
 
   // The stalker effect lights up the keys you've pressed recently
   StalkerEffect,
+
+    // We start with the LED effect that turns off all the LEDs.
+  LEDOff,
 
   // The numpad plugin is responsible for lighting up the 'numpad' mode
   // with a custom LED effect
